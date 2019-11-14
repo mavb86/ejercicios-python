2777fc047bb0b4ee220883ed49584ecfe069e048
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-14 16:36:20 +0100

    Create seccion3-ejercicio9.py

---
+++ seccion3-ejercicio9.py
@@ -0,0 +1,12 @@
+# Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuanto deberá pagar
+# finalmente por su compra.
+
+print("***************************")
+print("* SECCION 3 - EJERCICIO 9 *")
+print("***************************")
+
+importe = float(input("Introduzca el importe de la compra realizada:"))
+
+descuento = importe * 15 / 100
+
+print("El importe final de la compra es: ", importe - descuento)
