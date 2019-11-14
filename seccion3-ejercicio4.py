c05b74ae29fe1e780acca2123a6eb8f08c121fe3
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-14 16:07:09 +0100

    Create seccion3-ejercicio4.py

---
+++ seccion3-ejercicio4.py
@@ -0,0 +1,9 @@
+#Dados dos numeros, mostrar la suma, resta, división y multiplicación de ambos
+
+numero1 = float(input("Indique el primer numero para realizar los calculos :"))
+numero2 = float(input("Indique el segundo numero para realizar los calculos:"))
+
+print("El resultado de la suma es          : ", numero1 + numero2)
+print("El resultado de la resta es         : ", numero1 - numero2)
+print("El resultado de la multiplicacion es: ", numero1 * numero2)
+print("El resultado de la división es      : ", numero1 / numero2)
