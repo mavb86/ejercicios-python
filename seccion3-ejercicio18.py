bb9e36cdaa4f842f2d79e8ca8b8d119175dff460
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-14 17:29:43 +0100

    Create seccion3-ejercicio18.py

---
+++ seccion3-ejercicio18.py
@@ -0,0 +1,12 @@
+# Ejercicio 18
+# Pedir el nombre y los dos apellidos de una persona y mostrar las iniciales.
+
+print("****************************")
+print("* SECCION 3 - EJERCICIO 18 *")
+print("****************************")
+
+nombre = input("Introduzca su nombre: ")
+apellido1 = input("Introduzca su primer apellido: ")
+apellido2 = input("Introduzca su segundo apellido: ")
+
+print("Sus iniciales son ", nombre[0], " ", apellido1[0], " ", apellido2[0])
