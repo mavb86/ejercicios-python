75594de715323201adbd16f62e39bc9ce6cda56d
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-14 14:17:09 +0100

    Update seccion3-ejercicio2.py

--- seccion3-ejercicio2.py
+++ seccion3-ejercicio2.py
@@ -1,7 +1,10 @@
-# Calcular el perímetro y área de un rectángulo dada su base y su altura.
+#Calcular el perimetro y area de un rectangulo dada su base y altura
 
-base=float(input("Introduce la base:"))
-altura=float(input("Introduce la altura:"))
-perimetro = 2 * base * altura
-area = base * altura
-print("El perimetro es", perimetro, "y el area es", area)
+base   = float(input("Indica la base del rectangulo:"))
+altura = float(input("Indica la altura del rectangulo:"))
+
+perimetro = (base + altura) * 2
+area      = base * altura
+
+print("La perímetro calculado es: " + perimetro)
+print("La altura calculada es   : " + altura)
