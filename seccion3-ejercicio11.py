ed113bfc4f60db56a85716339dd4a1e6565dbfc5
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-14 16:52:32 +0100

    Create seccion3-ejercicio11.py

---
+++ seccion3-ejercicio11.py
@@ -0,0 +1,14 @@
+# Ejercicio 11
+# Pide al usuario dos números y muestra la “distancia” entre ellos (el valor absoluto de su diferencia,
+# de modo que el resultado sea siempre positivo).
+
+print("****************************")
+print("* SECCION 3 - EJERCICIO 11 *")
+print("****************************")
+
+numero1 = float(input("Escriba el primer número:"))
+numero2 = float(input("Escriba el segundo número:"))
+
+distancia = abs(numero1 - numero2)
+
+print("La distancia entre los dos números es: ", distancia)
