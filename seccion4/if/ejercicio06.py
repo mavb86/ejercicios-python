fd856b06b8f71d876d36127e467c4ac11198b4ae
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-15 11:21:14 +0100

    Create ejercicio06.py

---
+++ seccion4/if/ejercicio06.py
@@ -0,0 +1,10 @@
+# Ejercicio 6
+#
+# Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+
+cadena = input("Introduzca una cadena: ")
+
+if cadena == cadena.upper():
+    print("La cadena introducida esta en mayusculas")
+else:
+    print("La cadena introducida esta en minusculas")
