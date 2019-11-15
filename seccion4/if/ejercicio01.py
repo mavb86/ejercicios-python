78bf4ef73fde6ef418b6c2ed50838bef7bb81dd7
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-15 11:06:45 +0100

    Create ejercicio01.py

---
+++ seccion4/if/ejercicio01.py
@@ -0,0 +1,10 @@
+# Ejercicio 1
+# Algoritmo que pida dos números e indique si el primero es mayor que el segundo o no.
+
+num1 = float(input("Indique el número 1:"))
+num2 = float(input("Indique el número 2:"))
+
+if num1 > num2:
+    print("El primer número es mayor que el segundo")
+else:
+    print("El primer número no es mayor que el segundo")
