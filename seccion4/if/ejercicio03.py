7dfe38fc518a6f0e6251972445da73372549273c
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-15 11:11:46 +0100

    Create ejercicio03.py

---
+++ seccion4/if/ejercicio03.py
@@ -0,0 +1,10 @@
+# Ejercicio 3
+#
+# Escribe un programa que lea un número e indique si es par o impar.
+
+num = int(input("Indique un numero entero: "))
+
+if num % 2 == 0:
+    print("El número indicado es par")
+else:
+    print("El número indicado es impar")
