b41f00319dd0f8c7e635cb34604644c9ea73281a
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-17 20:18:52 +0100

    Create ejercicio10.py

---
+++ seccion4/bucles/ejercicio10.py
@@ -0,0 +1,8 @@
+# Ejercicio 10
+#
+# Algoritmo que muestre la tabla de multiplicar de los números 1,2,3,4 y 5.
+
+for i in range(1, 6):
+    for y in range(1, 11):
+        print(i, " x ", y, " = ", i * y)
+    print("*******************************************************************************************************")
