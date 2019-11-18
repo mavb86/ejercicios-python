b5927bd7e5f62175a9d551be4b53254cfe2f5660
mavb86 <57711930+mavb86@users.noreply.github.com>
2019-11-18 11:38:08 +0100

    Create ejercicio06.py

---
+++ seccion5/cadenas/ejercicio06.py
@@ -0,0 +1,8 @@
+# Ejercicio 6
+#
+# Realizar un programa que dada una cadena de caracteres por caracteres,
+# genere otra cadena resultado de invertir la primera.
+
+cad = input("Introduce una cadena y la invertiremos por usted: ")
+invertida = cad[::-1]
+print("La cadena invertida es:", invertida)
